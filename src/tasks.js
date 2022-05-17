e8ba97a56be07f5c49f44112affc29b94c5ff231
saya2022-code <s.nishizaka@g.dreamcareer.co.jp>
2022-05-17 13:49:07 +0900

    メモ機能(途中２)

--- src/tasks.js
+++ src/tasks.js
@@ -52,7 +52,7 @@ getTasks = async function () {
     connection = await mysql.createConnection(config.dbSetting);
     // ここに SQL を記述する
     const sql =
-      "SELECT t_task.id,t_task.task_name,t_task.deadline,t_task.category_id,m_category.category_name,t_task.task_status FROM t_task INNER JOIN m_category ON t_task.category_id = m_category.id";
+      "SELECT t_task.id, t_task.category_id, m_category.category_name, t_task.task_name, t_task.deadline, t_task.task_status, t_task.updated_at, t_task.created_at, t_task.memo FROM t_task LEFT JOIN m_category ON t_task.category_id = m_category.id;";
     const [rows, fields] = await connection.query(sql);
 
     return rows;
@@ -64,7 +64,7 @@ getTasks = async function () {
 };
 
 /**
- * タスクを１件削除する API
+ * タスクを削除する API
  *
  * @returns レスポンス JSON
  */
