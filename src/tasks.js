8eb5052adfcb4b160b3a43ce8fe79cd5b9866836
saya2022-code <s.nishizaka@g.dreamcareer.co.jp>
2022-05-17 14:43:51 +0900

    メモの展開表示(途中)

--- src/tasks.js
+++ src/tasks.js
@@ -52,7 +52,7 @@ getTasks = async function () {
     connection = await mysql.createConnection(config.dbSetting);
     // ここに SQL を記述する
     const sql =
-      "SELECT t_task.id, t_task.category_id, m_category.category_name, t_task.task_name, t_task.deadline, t_task.task_status, t_task.updated_at, t_task.created_at, t_task.memo FROM t_task LEFT JOIN m_category ON t_task.category_id = m_category.id;";
+      "SELECT t_task.id, t_task.category_id, m_category.category_name, t_task.task_name, t_task.deadline, t_task.task_status, t_task.updated_at, t_task.created_at, t_task.memo FROM t_task LEFT JOIN m_category ON t_task.category_id = m_category.id order by deadline;";
     const [rows, fields] = await connection.query(sql);
 
     return rows;
@@ -154,7 +154,7 @@ getTasks_status = async function (id) {
     connection = await mysql.createConnection(config.dbSetting);
     //ここにsqlを記述する↓
     const sql =
-      "SELECT t_task.id, t_task.category_id, m_category.category_name, t_task.task_name, t_task.deadline, t_task.task_status, t_task.updated_at, t_task.created_at FROM t_task LEFT JOIN m_category ON t_task.category_id = m_category.id WHERE  t_task.task_status = 1  order by deadline";
+      "SELECT t_task.id, t_task.category_id, m_category.category_name, t_task.task_name, t_task.deadline, t_task.task_status, t_task.updated_at, t_task.created_at, t_task.memo FROM t_task LEFT JOIN m_category ON t_task.category_id = m_category.id WHERE  t_task.task_status = 1  order by deadline;";
     const [rows, fields] = await connection.query(sql, id);
     return rows;
   } catch (err) {
